66a777ce20401df6a371a403e60012bec851989e
mattrubertone22 <38670002+mattrubertone22@users.noreply.github.com>
2021-04-25 23:38:40 -0400

    working comment capabilities

--- src/services/comment-service.js
+++ src/services/comment-service.js
@@ -1,41 +1,47 @@
 const COMMENTS_URL = "http://localhost:8080/api/comments";
-export const createComment = (teamId, commentObj) =>
-    fetch(`${COMMENTS_URL}/team/${teamId}/comments`, {
-        method: "POST",
-        body: JSON.stringify(commentObj),
-        headers: {
-            'content-type': 'application/json'
-        }
-    })
-        .then(response => response.json());
+export const createComment = (commentObj) =>
+  fetch(`${COMMENTS_URL}/team/${commentObj.uid}/comments`, {
+    method: "POST",
+    body: JSON.stringify(commentObj),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => response.json());
 
 export const deleteComment = (commentId) =>
-    fetch(`${COMMENTS_URL}/comments/${commentId}`, {
-        method: 'DELETE'
-    })
-        .then(response => response.json());
-
-
+  fetch(`${COMMENTS_URL}/comments/${commentId}`, {
+    method: "DELETE",
+  }).then((response) => response.json());
 
 export const updateComment = (commentId, comment) =>
-    fetch(`${COMMENTS_URL}/comments/${commentId}`, {
-        method: "PUT",
-        body: JSON.stringify(comment),
-        headers: {
-            'content-type': 'application/json'
-        }
-    })
-        .then(response => response.json());
+  fetch(`${COMMENTS_URL}/comments/${commentId}`, {
+    method: "PUT",
+    body: JSON.stringify(comment),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => response.json());
 
 export const findCommentsByUserName = (userName) =>
-    fetch(`${COMMENTS_URL}/users/${userName}/comments`).then(response => response.json())
+  fetch(`${COMMENTS_URL}/users/${userName}/comments`).then((response) =>
+    response.json()
+  );
 
 export const findCommentsByTeamId = (teamId) =>
-    fetch(`${COMMENTS_URL}/teams/${teamId}/comments`).then(response => response.json())
+  fetch(`${COMMENTS_URL}/teams/${teamId}/comments`).then((response) =>
+    response.json()
+  );
 
 export const findCommentsById = (commentId) =>
-    fetch(`${COMMENTS_URL}/comments/${commentId}`).then(response => response.json())
+  fetch(`${COMMENTS_URL}/comments/${commentId}`).then((response) =>
+    response.json()
+  );
 
 export default {
-    createComment, deleteComment, updateComment, findCommentsByUserName, findCommentsByTeamId, findCommentsById
-}
+  createComment,
+  deleteComment,
+  updateComment,
+  findCommentsByUserName,
+  findCommentsByTeamId,
+  findCommentsById,
+};
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,77 +1,76 @@
 const USER_API = "http://localhost:8080/api/users";
 
 const profile = () => {
-    return fetch(`${USER_API}/profile`, {
-        method: "POST",
-        credentials: "include"
-    }).then(response => response.json())
-}
-
+  return fetch(`${USER_API}/profile`, {
+    method: "GET",
+    credentials: "include",
+  }).then((response) => response.json());
+};
 
 const login = (credentials) => {
-    return fetch(`${USER_API}/login`, {
-        method: "POST",
-        credentials: "include",
-        body: JSON.stringify(credentials),
-        headers: {
-            'content-type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-}
+  return fetch(`${USER_API}/login`, {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify(credentials),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => response.json());
+};
 
 const logout = () => {
-    return fetch(`${USER_API}/logout`, {
-        method: "POST",
-        credentials: "include"
-    }).then(response => response.json())
-}
+  return fetch(`${USER_API}/logout`, {
+    method: "POST",
+    credentials: "include",
+  }).then((response) => response.json());
+};
 const getCurrentUser = () => {
-    return fetch(`${USER_API}/users/profile`,
-          {
-              method: "POST",
-              credentials: "include",
-              headers: {
-                  'content-type': 'application/json'
-              }
-          })
-        .then(response => response.text())
-        .then(responseText => responseText ? JSON.parse(responseText) : null)
-        }
+  return fetch(`${USER_API}/profile/{uid}`, {
+    method: "GET",
+    credentials: "include",
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then((response) => response.text())
+    .then((responseText) => (responseText ? JSON.parse(responseText) : null));
+};
 
 const signup = (credentials) => {
-    return fetch(`${USER_API}/signup`, {
-        method: "POST",
-        credentials: "include",
-        body: JSON.stringify(credentials),
-        headers: {
-            'content-type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-}
-
+  return fetch(`${USER_API}/signup`, {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify(credentials),
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => response.json());
+};
 
 const otherProfile = (userName) => {
+  console.log("userName inside the service:", userName);
+  return fetch(`${USER_API}/profile/${userName}`, {
+    method: "GET",
+  }).then((response) => response.json());
+};
 
-    return fetch(`${USER_API}/profile/${userName}`, {
-        method: "GET"
-    }).then(response => response.json())
-}
-
-
-
-const updateProfile = (currentUser)=>{
-    return fetch(`${USER_API}/profile`, {
-        method: "PUT",
-        body: JSON.stringify(currentUser),
-        credentials: "include",
-        headers: {
-            'content-type': 'application/json'
-        }
-    }).then(response => response.json())
-}
+const updateProfile = (currentUser) => {
+  return fetch(`${USER_API}/profile`, {
+    method: "PUT",
+    body: JSON.stringify(currentUser),
+    credentials: "include",
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then((response) => response.json());
+};
 
 export default {
-    signup, login, logout, profile,updateProfile, otherProfile,getCurrentUser
-}
+  signup,
+  login,
+  logout,
+  profile,
+  updateProfile,
+  otherProfile,
+  getCurrentUser,
+};
